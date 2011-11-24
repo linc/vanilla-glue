dc6db495a029f48aa7f3eabe4843e55a13bf4d9d
lincolnwebs <lincolnwebs@gmail.com>
2011-11-23 23:28:22 -0500

    Rename config to config-sample to prevent overwrites

--- wp-content/plugins/glue/config-sample.php
+++ wp-content/plugins/glue/config-sample.php
@@ -1,4 +1,5 @@
 <?php
+// Rename this file to config.php before uploading.
 
 // Full path to your Vanilla installation (no trailing slash)
 define('VANILLA_PATH', '/Users/destruction/Sites/intodetroit.dev');
