97b225aefbb52c5738c94f3b442fd203bd17902e
lincolnwebs <lincolnwebs@gmail.com>
2011-12-16 20:50:54 -0500

    No need to define cache path anymore

--- wp-content/plugins/glue/config-sample.php
+++ wp-content/plugins/glue/config-sample.php
@@ -3,6 +3,3 @@
 
 // Full path to your Vanilla installation (no trailing slash)
 define('VANILLA_PATH', '/path/to/vanilla');
-
-// Full path to Vanilla's cache folder
-define('PATH_CACHE', VANILLA_PATH.'/cache');
